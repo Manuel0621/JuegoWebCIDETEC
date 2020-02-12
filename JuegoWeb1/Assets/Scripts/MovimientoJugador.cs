84dcee5d8fcc6ed50c818201e1e08f80fa6fb2e8
Manuel <manu.navarro.18@gmail.com>
2020-02-12 17:30:02 -0600

    CreacionDelProyecto

---
+++ JuegoWeb1/Assets/Scripts/MovimientoJugador.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using NDream.AirConsole;
+using Newtonsoft.Json.Linq;
+
+public class MovimientoJugador : MonoBehaviour
+{
+
+    private Rigidbody rb;
+    public float velocidad;
+    public float multiplicador;
+    public bool iniciado;
+    private float x;
+    private bool moviendose;
+    private bool presionado;
+    private bool soltado;
+
+    private void Awake()
+    {
+        AirConsole.instance.onMessage += OnMessage;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        velocidad = 0;
+        iniciado = false;
+        presionado = false;
+        soltado = false;
+    }
+    void OnMessage(int fromDeviceID, JToken data)
+    {
+        Debug.Log("message from" + fromDeviceID + ", data:" + data);
+        if (data["action"] != null && data["action"].ToString().Equals("presionado"))
+        {
+            presionado = true;
+            soltado = false;
+            //Camera.main.backgroundColor = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
+        }
+        if (data["action"] != null && data["action"].ToString().Equals("soltado"))
+        {
+            presionado = false;
+            soltado = true;
+            //Camera.main.backgroundColor = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (presionado==true)
+        {
+            if (velocidad < 85)
+            {
+                //print("up arrow key is held down");
+                velocidad += 1;
+                //rb.drag +=0.01f;
+                print(velocidad);
+            }
+            
+        }
+
+    }
+    private void FixedUpdate()
+    {
+        //moviendose = EnMovimiento();
+        if (!EnMovimiento())
+        {
+            if (soltado==true)
+            {
+                Vector3 movimiento = new Vector3(-velocidad * multiplicador, 0.0f, 0.0f);
+                rb.AddForce(movimiento);
+                velocidad = 0;
+                soltado = false;
+                if (iniciado == false)
+                {
+                    iniciado = true;
+                }
+            }
+        }
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("CajaSpawn"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+    bool EnMovimiento()
+    {
+            if (transform.position.x == x)
+            {
+                x = transform.position.x;
+                return false;
+
+            }
+            else
+            {
+                x = transform.position.x;
+                return true;
+            }
+    }
+
+
+}
---
+++ JuegoWeb1/Assets/Scripts/spawnDeEscenario.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawnDeEscenario : MonoBehaviour
+{
+    public GameObject Escenario1;
+    public GameObject Escenario2;
+    public GameObject Escenario3;
+    public GameObject Escenario4;
+    public GameObject Escenario5;
+    public GameObject Escenario6;
+    private int EscenarioAPoner;
+    public GameObject PuntoSpawn;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Jugador"))
+        {
+            PonerSecciones();
+        }
+    }
+    private void PonerSecciones()
+    {
+        int x = 0;
+        for (int i = 0; i < 6; i++)
+        {
+            GameObject nuevaPlataforma;
+            EscenarioAPoner = Random.Range(0,7);
+
+            switch (EscenarioAPoner)
+            {
+                case 0:
+                    nuevaPlataforma = Instantiate(Escenario1);
+                    nuevaPlataforma.transform.position = PuntoSpawn.transform.position - new Vector3(x: x, y: 1.5f, z: 0);
+                    //x = x+6;
+                    break;
+                case 1:
+                    nuevaPlataforma = Instantiate(Escenario2);
+                    nuevaPlataforma.transform.position = PuntoSpawn.transform.position - new Vector3(x: x, y: 1.5f, z: 0);
+                    //x= x+6;
+                    break;
+                case 2:
+                    nuevaPlataforma = Instantiate(Escenario3);
+                    nuevaPlataforma.transform.position = transform.position + new Vector3(x: x, y: -1.5f, z: 0);
+                   //x -= 6;
+                    break;
+                case 3:
+                    nuevaPlataforma = Instantiate(Escenario4);
+                    nuevaPlataforma.transform.position = transform.position + new Vector3(x: x, y: -1.5f, z: 0);
+                   // x -= 6;
+                    break;
+                case 4:
+                    nuevaPlataforma = Instantiate(Escenario5);
+                    nuevaPlataforma.transform.position = transform.position + new Vector3(x: x, y: -1.5f, z: 0);
+                   // x -= 6;
+                    break;
+                case 5:
+                    nuevaPlataforma = Instantiate(Escenario6);
+                    nuevaPlataforma.transform.position = transform.position + new Vector3(x: x, y: -1.5f, z: 0);
+                    //x -= 6;
+                    break;
+                default:
+                    nuevaPlataforma = Instantiate(Escenario1);
+                    nuevaPlataforma.transform.position = PuntoSpawn.transform.position - new Vector3(x: x, y: 1.5f, z: 0);
+                    //x =x+6;
+                    break;
+            }
+        }
+    }
+}
